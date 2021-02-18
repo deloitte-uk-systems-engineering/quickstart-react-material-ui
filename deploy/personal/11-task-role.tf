30954b954e42c6806154b35af24b94817b376aff
Matthew Lau <lau.cy.matthew@gmail.com>
2021-02-18 11:48:19 +0000

    Add deploy directory

---
+++ deploy/personal/11-task-role.tf
@@ -0,0 +1,52 @@
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs-staging-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+resource "aws_iam_role_policy" "ecs_task_execution_role" {
+  name = "ecs_task_execution_policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+        ],
+        "Resource": "*"
+    },
+     {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${var.mongo_password_secret_arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
---
+++ deploy/personal/terraform.tfvars
@@ -0,0 +1,6 @@
+default_region            = "us-east-1"
+docker_username           = "matlau"
+github_username           = "MatthewCYLau"
+github_project_name       = "quickstart-react-material-ui"
+app_name                  = "quickstart-react-material-ui-app"
+environment               = "staging"

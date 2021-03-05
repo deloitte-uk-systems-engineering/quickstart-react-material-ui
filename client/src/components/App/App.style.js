6c082c96bf1c100da7832718cba0d1c28ec19456
Ricky McGeehan <rmcgeehan@deloitte.co.uk>
2021-03-05 10:28:43 +0000

    Adding sample routing to app

--- client/src/components/App/App.style.js
+++ client/src/components/App/App.style.js
@@ -1,57 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({
+export default makeStyles(({ palette }) => ({
     root: {
         height: '100%',
-        color: theme.palette.text.primary,
-        backgroundColor: theme.palette.background.paper,
+        color: palette.text.primary,
+        backgroundColor: palette.background.paper,
         display: 'flex',
         flexDirection: 'column'
-    },
-    main: {
-        flex: 1,
-        display: 'flex',
-        overflow: 'auto',
-        paddingTop: theme.spacing(3),
-        paddingBottom: theme.spacing(3)
-    },
-    content: {
-        margin: 'auto',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        [theme.breakpoints.up('md')]: {
-            flexDirection: 'row',
-        }
-    },
-    section: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    image: {
-        maxWidth: 300,
-        width: '100%',
-        marginBottom: theme.spacing(3),
-        [theme.breakpoints.up('md')]: {
-            maxWidth: 400,
-            marginBottom: 0,
-            marginRight: theme.spacing(3)
-        }
-    },
-    paragraph: {
-        display: 'flex',
-        '&:last-child': {
-            marginBottom: 0
-        }
-    },
-    icon: {
-        marginRight: theme.spacing(2),
-        fontSize: '2.8rem'
-    },
-    link: {
-        fontWeight: '500'
     }
 }),
 { name: 'App' });
--- client/src/config/Theme.js
+++ client/src/config/Theme.js
@@ -5,15 +5,15 @@ import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes';
 export default (darkMode) => {
     const palette = createPalette({
         /**
-        * In this example, the primary and secondary colours have been overwritten
-        * using the Deloitte colour palette. When defining custom colours, the only
-        * shade that must be defined is 'main' - all other colours (i.e. 'light',
-        * 'dark' and 'contrastText') will be augmented from that colour. However, as
-        * you can see below, these colours can also be explicitly defined.
-        *
-        * See: https://material-ui.com/customization/palette/
-        * See: https://material-ui.com/customization/color/
-        */
+         * In this example, the primary and secondary colours have been overwritten
+         * using the Deloitte colour palette. When defining custom colours, the only
+         * shade that must be defined is 'main' - all other colours (i.e. 'light',
+         * 'dark' and 'contrastText') will be augmented from that colour. However, as
+         * you can see below, these colours can also be explicitly defined.
+         *
+         * See: https://material-ui.com/customization/palette/
+         * See: https://material-ui.com/customization/color/
+         */
         primary: {
             main: '#86BC25',
             light: '#C4D600',
@@ -38,21 +38,37 @@ export default (darkMode) => {
     });
 
     /**
-    * The default Material UI theme can be extended/overwritten by passing
-    * options into the 'createMuiTheme' function. In this example, only the
-    * palette is being overwritten, however there are many other parts of the
-    * theme that can be customised including spacing, typography and screen
-    * breakpoints.
-    *
-    * See: https://material-ui.com/customization/theming/
-    * See: https://material-ui.com/customization/default-theme/
-    */
+     * The default Material UI theme can be extended/overwritten by passing
+     * options into the 'createMuiTheme' function. In this example, only the
+     * palette is being overwritten, however there are many other parts of the
+     * theme that can be customised including spacing, typography and screen
+     * breakpoints.
+     *
+     * See: https://material-ui.com/customization/theming/
+     * See: https://material-ui.com/customization/default-theme/
+     */
     const theme = createMuiTheme({ palette });
 
     /**
-    * Generate responsive typography settings.
-    *
-    * See: https://material-ui.com/customization/theming/#responsivefontsizes-theme-options-theme
-    */
+     * You can take advantage of the overrides key of the theme object to
+     * change any style injected by Material-UI into the DOM. In this example,
+     * the styles for the Material-UI Link component has been overridden to
+     * have a bolder font weight.
+     *
+     * See: https://material-ui.com/customization/globals/#css
+     */
+    theme.overrides = {
+        MuiLink: {
+            root: {
+                fontWeight: 500
+            }
+        }
+    };
+
+    /**
+     * Generate responsive typography settings.
+     *
+     * See: https://material-ui.com/customization/theming/#responsivefontsizes-theme-options-theme
+     */
     return responsiveFontSizes(theme);
 };
